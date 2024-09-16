0b214b60ccacd796f9defa49761702163b7ffc6f
Netra Luis Castells <netraluis@gmail.com>
2024-09-16 09:59:54 +0900

    client helpers added

--- lib/domain/clientHelpers.ts
+++ lib/domain/clientHelpers.ts
@@ -1,27 +1,27 @@
 export const getDomain = async (domain: string) => {
   console.log("Fetching domain data for:", domain);
   try {
-    // const response = await fetch(`/api/domain/${domain}`, {
-    //   method: "GET",
-    // });
-    // const domainData = await response.json();
-    // console.log("Domain data:", domainData);
-    const domainData = {
-      domain: {
-        assistantId: "asst_lwr5WIVDFjoV8pL0CHic2BFd",
-        assistantName: "AI Andorra UE",
-        createdAt: "2024-09-15T07:40:15.585Z",
-        customDomain: "null",
-        id: "fm11ujxfx0000137h7qmc5f73",
-        logo: "https://sjgdbtgjgkkmztduxohh.supabase.co/storage/v1/object/public/images/logos/fm11ujxfx0000137h7qmc5f73.png",
+    const response = await fetch(`/api/domain/${domain}`, {
+      method: "GET",
+    });
+    const domainData = await response.json();
+    console.log("Domain data:", domainData);
+    // const domainData = {
+    //   domain: {
+    //     assistantId: "asst_lwr5WIVDFjoV8pL0CHic2BFd",
+    //     assistantName: "AI Andorra UE",
+    //     createdAt: "2024-09-15T07:40:15.585Z",
+    //     customDomain: "null",
+    //     id: "fm11ujxfx0000137h7qmc5f73",
+    //     logo: "https://sjgdbtgjgkkmztduxohh.supabase.co/storage/v1/object/public/images/logos/fm11ujxfx0000137h7qmc5f73.png",
 
-        menufooter: "Fet amb 🖤  a Andorra i per andorra",
-        name: "andorra UE",
-        subDomain: "andorraue",
-        welcome: "Benvingut a Andorra UE",
-      },
-      status: 200,
-    };
+    //     menufooter: "Fet amb 🖤  a Andorra i per andorra",
+    //     name: "andorra UE",
+    //     subDomain: "andorraue",
+    //     welcome: "Benvingut a Andorra UE",
+    //   },
+    //   status: 200,
+    // };
     return domainData;
   } catch (error) {
     console.error("Error fetching domain token:", error);
