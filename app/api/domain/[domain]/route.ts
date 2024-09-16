a37394410cd090b98bf38b8ae5dee8906ba95f96
Netra Luis Castells <netraluis@gmail.com>
2024-09-16 10:30:34 +0900

    primsa back again

--- app/api/domain/[domain]/route.ts
+++ app/api/domain/[domain]/route.ts
@@ -1,5 +1,15 @@
 import { getDomainData } from "../../../../lib/domain/serverHelpers";
 
+export async function OPTIONS() {
+  return new Response(null, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+  });
+}
+
 export async function GET(
   req: Request,
   { params }: { params: { domain: string } },
@@ -7,7 +17,15 @@ export async function GET(
   try {
     const domain = await getDomainData(params.domain);
 
-    return Response.json({ domain, status: 200 });
+    return Response.json(
+      { domain, status: 200 },
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*", // Enable CORS for all origins
+          "Content-Type": "application/json",
+        },
+      },
+    );
   } catch (error) {
     console.error("Error retrieving access token:", error);
 
