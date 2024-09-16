72310039826b72623b8ec512cd99c27d6313579f
Netra Luis Castells <netraluis@gmail.com>
2024-09-16 13:55:16 +0900

    sin usar imports

--- app/[domain]/layout.tsx
+++ app/[domain]/layout.tsx
@@ -1,4 +1,5 @@
 "use server";
+import prisma from "../../lib/prisma";
 import { ReactNode } from "react";
 import { notFound, redirect } from "next/navigation";
 // import { getDomainData } from "@/lib/fetchers";
@@ -7,6 +8,44 @@ import { Metadata } from "next";
 import "../globals.css";
 import { GlobalProvider } from "@/components/context/globalContext";
 import Header from "@/components/header";
+import { createClient } from "@/lib/supabase/server";
+
+const getPublicUrlImageimport = async (fileName: string) => {
+  const supabase = createClient();
+
+  const { data } = supabase.storage.from("images").getPublicUrl(fileName);
+
+  return data.publicUrl;
+};
+
+async function getDomainData(domain: string) {
+  console.log("Domain:", domain, process.env.NEXT_PUBLIC_ROOT_DOMAIN);
+  const rootDomain =
+    `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}` || ".localhost:3000";
+  const subdomain = domain.endsWith(rootDomain)
+    ? domain.replace(rootDomain, "")
+    : null;
+
+  console.log("Subdomain:", subdomain);
+
+  const subdomainInfo = await prisma.domains.findFirst({
+    where: subdomain
+      ? { subDomain: subdomain } // If subdomain is true, filter by subdomain
+      : { customDomain: domain }, // Otherwise, filter by customDomain
+  });
+
+  if (!subdomainInfo) {
+    return null;
+  }
+
+  const logo = await getPublicUrlImageimport(`logos/${subdomainInfo.id}.png`);
+  console.log("Logo:", logo);
+
+  return {
+    ...subdomainInfo,
+    logo,
+  };
+}
 
 export async function generateMetadata({
   params,
@@ -14,23 +53,23 @@ export async function generateMetadata({
   params: { domain: string };
 }): Promise<Metadata | null> {
   const domain = decodeURIComponent(params.domain);
-  // const data = await getDomainData(domain);
+  const data = await getDomainData(domain);
 
-  const subdomainInfo = {
-    assistantId: "asst_lwr5WIVDFjoV8pL0CHic2BFd",
-    assistantName: "AI Andorra UE",
-    createdAt: "2024-09-15T07:40:15.585Z",
-    customDomain: "null",
-    id: "fm11ujxfx0000137h7qmc5f73",
-    logo: "https://sjgdbtgjgkkmztduxohh.supabase.co/storage/v1/object/public/images/logos/fm11ujxfx0000137h7qmc5f73.png",
+  // const subdomainInfo = {
+  //   assistantId: "asst_lwr5WIVDFjoV8pL0CHic2BFd",
+  //   assistantName: "AI Andorra UE",
+  //   createdAt: "2024-09-15T07:40:15.585Z",
+  //   customDomain: "null",
+  //   id: "fm11ujxfx0000137h7qmc5f73",
+  //   logo: "https://sjgdbtgjgkkmztduxohh.supabase.co/storage/v1/object/public/images/logos/fm11ujxfx0000137h7qmc5f73.png",
 
-    menufooter: "Fet amb 🖤  a Andorra i per andorra",
-    name: "andorra UE",
-    subDomain: "andorraue",
-    welcome: "Benvingut a Andorra UE",
-  };
+  //   menufooter: "Fet amb 🖤  a Andorra i per andorra",
+  //   name: "andorra UE",
+  //   subDomain: "andorraue",
+  //   welcome: "Benvingut a Andorra UE",
+  // };
 
-  const data = subdomainInfo;
+  // const data = subdomainInfo;
   // const data = {
   //   name: "test",
   //   description: "test",
