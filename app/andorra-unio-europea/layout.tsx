39a2275c2bcf215be71a24338aaf9f40d625ce82
Netra Luis Castells <netraluis@gmail.com>
2024-09-28 19:05:55 +0900

    layout ue update

--- app/andorra-unio-europea/layout.tsx
+++ app/andorra-unio-europea/layout.tsx
@@ -1,21 +1,80 @@
 "use server";
+import { ReactNode } from "react";
+import { notFound, redirect } from "next/navigation";
+import { Metadata } from "next";
 import "../globals.css";
-import Header from "../../components/header";
-import { GlobalProvider } from "../../components/context/globalContext";
+import { GlobalProvider } from "@/components/context/globalContext";
+import Header from "@/components/header";
+import { getDomainData } from "@/lib/domain/serverHelpers";
 
-export default async function RootLayout({
+export async function generateMetadata({
+  params,
+}: {
+  params: { domain: string };
+}): Promise<Metadata | null> {
+  const domain = decodeURIComponent(params.domain);
+  const data = await getDomainData(domain);
+
+  if (!data) {
+    return null;
+  }
+
+  return {
+    title: `${data.name}`,
+    description: data.description || "",
+    openGraph: {
+      title: data.name,
+      description: data.description || "",
+      images: [data.logo],
+    },
+    icons: {
+      icon: [{ url: data.symbol ? data.symbol : "/chatbotforSymbol.svg" }], // Añade el tipo de imagen
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: data.name,
+      description: data.description || "",
+      images: [data.logo],
+      creator: "netraluis and anton odena",
+    },
+    metadataBase: new URL(`https://${domain}`),
+    // Optional: Set canonical URL to custom domain if it exists
+    // ...(params.domain.endsWith(`.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) &&
+    //   data.customDomain && {
+    //     alternates: {
+    //       canonical: `https://${data.customDomain}`,
+    //     },
+    //   }),
+  };
+}
+
+export default async function SiteLayout({
+  params,
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  params: { domain: string };
+  children: ReactNode;
+}) {
+  const domain = decodeURIComponent(params.domain);
+  const data = await getDomainData(domain);
+
+  if (!data) {
+    notFound();
+  }
+
+  // Optional: Redirect to custom domain if it exists
+  if (
+    domain.endsWith(`.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) &&
+    data.customDomain &&
+    process.env.REDIRECT_TO_CUSTOM_DOMAIN_IF_EXISTS === "true"
+  ) {
+    return redirect(`https://${data.customDomain}`);
+  }
+
   return (
-    <html lang="en">
-      <GlobalProvider>
-        <body>
-          <Header domain="andorraue.localhost:3000" />
-          {children}
-        </body>
-      </GlobalProvider>
-    </html>
+    <GlobalProvider>
+      <Header domain={domain} />
+      <div>{children}</div>
+    </GlobalProvider>
   );
 }
