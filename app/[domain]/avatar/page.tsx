0638d284b08623e61b9f7dc3e082f6fb755cef6b
Netra Luis Castells <netraluis@gmail.com>
2024-10-02 15:35:39 +0900

    undo avatar security

--- app/[domain]/avatar/page.tsx
+++ app/[domain]/avatar/page.tsx
@@ -1,15 +1,14 @@
 "use client";
 import InteractiveAvatar from "@/components/InteractiveAvatar";
 import React from "react";
-const AVATAR_ACTIVE = process.env.AVATAR_ACTIVE;
 
 export default function ChatComponent() {
-  if (!AVATAR_ACTIVE) {
-    return (
-      <div className="flex">
-        <>no tiene un avatar asociado</>
-      </div>
-    );
-  }
+  // if (!AVATAR_ACTIVE) {
+  //   return (
+  //     <div className="flex">
+  //       <>no tiene un avatar asociado</>
+  //     </div>
+  //   );
+  // }
   return <InteractiveAvatar />;
 }
