8505beead7ed46d100bdef583c4758583eeec9d0
Netra Luis Castells <netraluis@gmail.com>
2024-10-02 15:27:02 +0900

    add sec to avatar show

--- app/[domain]/avatar/page.tsx
+++ app/[domain]/avatar/page.tsx
@@ -1,13 +1,15 @@
 "use client";
-import { GlobalContext } from "@/components/context/globalContext";
 import InteractiveAvatar from "@/components/InteractiveAvatar";
-import React, { useContext } from "react";
-
+import React from "react";
+const AVATAR_ACTIVE = process.env.AVATAR_ACTIVE;
 
 export default function ChatComponent() {
-  const { domainData } = useContext(GlobalContext);
-  if (domainData?.assistantId){
-    return <div className="flex"><>no tiene un avatar asociado</></div>
+  if (!AVATAR_ACTIVE) {
+    return (
+      <div className="flex">
+        <>no tiene un avatar asociado</>
+      </div>
+    );
   }
   return <InteractiveAvatar />;
 }
