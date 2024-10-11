6835385dd6fac23aa73be438363cdfa018df8498
Netra Luis Castells <netraluis@gmail.com>
2024-10-11 10:04:36 +0900

    change footer text

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,12 +11,17 @@ export function FooterText({ className, ...props }: FooterTextProps) {
     <p
       className={cn(
         "px-2 text-center text-xs leading-normal text-muted-foreground",
-        className,
+        className
       )}
       {...props}
     >
-      Totes les respostes d’aquesta conversa estan generades mitjançant una
-      Intel·ligència Artificial (AI)
+      {/* Totes les respostes d’aquesta conversa estan generades mitjançant una
+      Intel·ligència Artificial (AI) */}
+      Genero respostes amb intel·ligència artificial i puc cometre errors. Si
+      vols confirmar la informació, pots posar-te en contacte amb els tècnics de
+      la Secretaria d'Estat per a les Relacions amb la Unió Europea o bé
+      contactar-nos andorraue@govern.ad, +376 875 700 o a través de WhatsApp:
+      +376 637 400
     </p>
   );
 }
