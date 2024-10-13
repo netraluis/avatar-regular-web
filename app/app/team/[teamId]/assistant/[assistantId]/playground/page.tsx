fa4b1c2d3afa1881db56f18dd0914163b8448233
Netra Luis Castells <netraluis@gmail.com>
2024-10-13 13:21:55 +0900

    redirect to playground build fix

--- app/app/team/[teamId]/assistant/[assistantId]/playground/page.tsx
+++ app/app/team/[teamId]/assistant/[assistantId]/playground/page.tsx
@@ -1,19 +1,6 @@
 "use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 
 const Playground = () => {
-  const router = useRouter();
-
-  //   useEffect(() => {
-  //     const currentPath = window.location.pathname;
-
-  //     // Concatenar el nuevo path con la ruta actual
-  //     const newPath = `${currentPath}/playground`;
-
-  //     return router.push(newPath);
-  //   }, []);
-
   return <div>Playground...</div>;
 };
 
