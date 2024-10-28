0fe1b8c24b185b52f5cb4ad76fbda868de019135
Netra Luis Castells <netraluis@gmail.com>
2024-10-28 21:54:49 +0100

    get parameters

--- app/api/send-emails/route.ts
+++ app/api/send-emails/route.ts
@@ -1,21 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  const SUPABASE_HOOK_SECRET = process.env.SEND_EMAIL_HOOK_SECRET; // Asegúrate de que esto esté definido en tu .env
+  // const SUPABASE_HOOK_SECRET = process.env.SEND_EMAIL_HOOK_SECRET; // Asegúrate de que esto esté definido en tu .env
   try {
     // Extrae el secreto del encabezado de la solicitud
-    const secret = request.headers.get("x-secret-key");
+    // const secret = request.headers.get("x-secret-key");
 
     console.log(request.headers)
     const data = await request.json();
     console.log({data});
 
     // Verifica si el secreto coincide con el configurado en Supabase
-    if (!secret || secret !== SUPABASE_HOOK_SECRET) {
-      return new NextResponse("Unauthorized", {
-        status: 401,
-      });
-    }
+    // if (!secret || secret !== SUPABASE_HOOK_SECRET) {
+    //   return new NextResponse("Unauthorized", {
+    //     status: 401,
+    //   });
+    // }
+
+    const url = new URL(request.url);
+    const secretQuery = url.searchParams.get("secret");
+
+    console.log("Parámetro 'secret' en query:", secretQuery, url);
 
     // Procesa la solicitud después de la verificación del secreto
     console.log("Solicitud autorizada desde Supabase Hook");
