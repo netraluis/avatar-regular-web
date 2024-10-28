806499c3ad21d0430f4cb4e5d1e76b1906946bd4
Netra Luis Castells <netraluis@gmail.com>
2024-10-28 22:53:17 +0100

    fix

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -42,7 +42,7 @@ export async function updateSession(request: NextRequest) {
     !request.nextUrl.pathname.startsWith("/login") &&
     !request.nextUrl.pathname.startsWith("/signup") &&
     !request.nextUrl.pathname.startsWith("/confirm") &&
-    !request.nextUrl.pathname.startsWith("/api/auth/") &&
+    !request.nextUrl.pathname.startsWith("/api/auth") &&
     !request.nextUrl.pathname.startsWith("/api/send-emails")
   ) {
     return NextResponse.redirect(new URL("/login", request.url));
