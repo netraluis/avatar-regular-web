3359f8da6510e8d3a27d36b8452205c98c72d6d1
Netra Luis Castells <netraluis@gmail.com>
2024-11-24 23:35:40 +0100

    delete console log

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -4,8 +4,6 @@ import { cookies } from "next/headers";
 export function createClient() {
   const cookieStore = cookies();
 
-  console.log({NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY})
-
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
