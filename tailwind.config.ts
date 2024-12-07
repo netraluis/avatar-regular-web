f751e3afdf0afcc25864fc2c5a864a82c0abfaa4
Netra Luis Castells <netraluis@gmail.com>
2024-12-07 18:54:56 +0100

    tailwind style udpate

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -72,6 +72,9 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      width: {
+        'redirect': '375px',
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
