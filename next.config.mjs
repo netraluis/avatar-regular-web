6f8512ca8bfe5ae23e933d0ec3fc3ac3fb95f061
Netra Luis Castells <netraluis@gmail.com>
2024-12-17 23:44:54 +0100

    config images

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,10 @@ const nextConfig = {
   images: {
     domains: [
       "sjgdbtgjgkkmztduxohh.supabase.co",
-      "https://sjgdbtgjgkkmztduxohh.supabase.co",
       "dkwrylibzobzgzzddull.supabase.co",
-      "https://dkwrylibzobzgzzddull.supabase.co",
       "dkwrylibzobzgzzddull.supabase.co",
-      "https://dkwrylibzobzgzzddull.supabase.co",
       "qlguauwpvmmybiacbggc.supabase.co",
-      "https://qlguauwpvmmybiacbggc.supabase.co",
       "iaqhmgdahgeulbjmjksa.supabase.co",
-      "https://iaqhmgdahgeulbjmjksa.supabase.co",
     ],
     dangerouslyAllowSVG: true,
   },
