cd124b8080152541132897e3fdff6bc2c96cbf53
Netra Luis Castells <netraluis@gmail.com>
2024-12-19 13:59:50 +0100

    test paddle

--- app/app/(dashboard)/team/[teamId]/settings/plans/page.tsx
+++ app/app/(dashboard)/team/[teamId]/settings/plans/page.tsx
@@ -1,13 +1,93 @@
-import { Check, HelpCircle } from "lucide-react";
+'use client'
+import { Check } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
+import { Environments, initializePaddle, Paddle } from '@paddle/paddle-js';
+import { useEffect, useState } from "react";
+
+
+// import {
+//   Tooltip,
+//   TooltipContent,
+//   TooltipProvider,
+//   TooltipTrigger,
+// } from "@/components/ui/tooltip";
+
+const plans = {
+  title: "Preus i plans",
+  description: "Tria el pla que millor s’adapti a tu!",
+  cards: [
+    {
+      title: "Hobby",
+      price: 19,
+      monthly: "al mes",
+      includedPlus: "Tot el que inclou el pla gratuït, a més…",
+      allIncluded: "All features from the free plan included",
+      features: [
+        "Accés a models avançats",
+        "2.000 crèdits de missatges/mes",
+        "2 xatbots",
+        "11.000.000 caràcters per xatbot",
+        "Enllaços il·limitats per entrenar",
+        "Accés a l’API",
+        "Integracions",
+        "Analítiques bàsiques",
+      ],
+    },
+    {
+      title: "Estàndard",
+      price: 99,
+      monthly: "al mes",
+      includedPlus: "Tot el que inclou el pla Hobby, a més…",
+      allIncluded: "All features from the free plan included",
+      features: [
+        "10.000 crèdits de missatges/mes",
+        "5 xatbots",
+        "3 team members",
+      ],
+    },
+    {
+      title: "Il·limitat",
+      price: 399,
+      monthly: "al mes",
+      includedPlus: "Tot el que inclou el pla gratuït, a més…",
+      allIncluded: "All features from the free plan included",
+      features: [
+        "40.000 crèdits de missatges/mes (els missatges que superin aquest límit utilitzaran la teva clau d’API d’OpenAI)",
+        "10 xatbots",
+        "5 membres de l’equip",
+        "Elimina “Powered by Chatbotfor",
+        "Usa els teus propis dominis personalitzats",
+        "Analítiques avançades",
+      ],
+    },
+  ],
+}
 
 export default function Component() {
+  const [paddle, setPaddle] = useState<Paddle>();
+
+  useEffect(() => {
+    if (!paddle?.Initialized && process.env.NEXT_PUBLIC_PADDLE_CLIENT_TOKEN && process.env.NEXT_PUBLIC_PADDLE_ENV) {
+      initializePaddle({ environment: process.env.NEXT_PUBLIC_PADDLE_ENV as Environments, token: process.env.NEXT_PUBLIC_PADDLE_CLIENT_TOKEN }).then(
+        (paddleInstance: Paddle | undefined) => {
+          if (paddleInstance) {
+            setPaddle(paddleInstance);
+          }
+        },
+      )
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log(paddle);
+  }, [paddle]);
+
+  const openCheckout = () => {
+    paddle?.Checkout.open({
+      items: [{ priceId: 'pro_01jff3rxysjzgm9fy8fjcr1rch', quantity: 1 }],
+    });
+  };
+
   return (
     <div className="max-w-6xl mx-auto px-4 py-8">
       <div className="text-center mb-10">
@@ -16,157 +96,47 @@ export default function Component() {
           Tria el pla que millor s’adapti a tu!
         </p>
       </div>
-
       <div className="grid md:grid-cols-3 gap-8">
-        <Card>
-          <CardHeader className="space-y-1">
-            <CardTitle>
-              <h2 className="text-xl font-bold">Hobby</h2>
-            </CardTitle>
-            <div className="flex items-baseline justify-between">
-              <span className="text-4xl font-bold">19 $</span>
-              <span className="text-sm text-muted-foreground">Al mes</span>
-            </div>
-          </CardHeader>
-          <CardContent className="h-full">
-            <div className="h-full flex flex-col">
-              <div className="space-y-4">
-                <div className="flex items-center gap-2 text-sm text-muted-foreground">
-                  Tot el que inclou el pla gratuït, a més…
-                  <TooltipProvider>
+        {plans.cards.map((plan, index) => (
+          <Card key={index} onClick={openCheckout} className="cursor-pointer">
+            <CardHeader className="space-y-1">
+              <CardTitle>
+                {plan.title}
+              </CardTitle>
+              <div className="flex items-baseline justify-between">
+                <span className="text-4xl font-bold">{plan.price} €</span>
+                <span className="text-sm text-muted-foreground">{plan.monthly}</span>
+              </div>
+            </CardHeader>
+            <CardContent className="h-full">
+              <div className="h-full flex flex-col">
+                <div className="space-y-4">
+                  <div className="flex items-center gap-2 text-sm text-muted-foreground">
+                    {plan.includedPlus}
+                    {/* <TooltipProvider>
                     <Tooltip>
                       <TooltipTrigger>
                         <HelpCircle className="w-4 h-4" />
                       </TooltipTrigger>
                       <TooltipContent>
-                        <p>All features from the free plan included</p>
+                        <p>{plan.allIncluded}</p>
                       </TooltipContent>
                     </Tooltip>
-                  </TooltipProvider>
+                  </TooltipProvider> */}
+                  </div>
+                  <ul className="space-y-2.5 pt-4">
+                    {plan.features.map((feature, index) => (
+                      <li key={index} className="flex items-center gap-2">
+                        <Check className="w-4 h-4" />
+                        <span>{feature}</span>
+                      </li>
+                    ))}
+                  </ul>
                 </div>
-                <ul className="space-y-2.5 pt-4">
-                  <li className="flex items-center gap-2">
-                    <Check className="w-4 h-4" />
-                    <span>Accés a models avançats</span>
-                  </li>
-                  <li className="flex items-center gap-2">
-                    <Check className="w-4 h-4" />
-                    <span>2.000 crèdits de missatges/mes</span>
-                  </li>
-                  <li className="flex items-center gap-2">
-                    <Check className="w-4 h-4" />
-                    <span>2 xatbots</span>
-                  </li>
-                  <li className="flex items-center gap-2">
-                    <Check className="w-4 h-4" />
-                    <span>11.000.000 caràcters per xatbot</span>
-                  </li>
-                  <li className="flex items-center gap-2">
-                    <Check className="w-4 h-4" />
-                    <span>Enllaços il·limitats per entrenar</span>
-                  </li>
-                  <li className="flex items-center gap-2">
-                    <Check className="w-4 h-4" />
-                    <span>Accés a l’API</span>
-                  </li>
-                  <li className="flex items-center gap-2">
-                    <Check className="w-4 h-4" />
-                    <span>Integracions</span>
-                  </li>
-                  <li className="flex items-center gap-2">
-                    <Check className="w-4 h-4" />
-                    <span>Analítiques bàsiques</span>
-                  </li>
-                </ul>
               </div>
-            </div>
-          </CardContent>
-        </Card>
-
-        <Card className="border-primary">
-          <CardHeader className="space-y-1">
-            <CardTitle>
-              <h2 className="text-xl font-bold">Estàndard</h2>
-            </CardTitle>
-            <div className="flex items-baseline justify-between">
-              <span className="text-4xl font-bold">$99</span>
-              <span className="text-sm text-muted-foreground">Al mes</span>
-            </div>
-          </CardHeader>
-          <CardContent className="h-full">
-            <div className="h-full flex flex-col">
-              <div className="space-y-4">
-                <div className="text-sm text-muted-foreground">
-                  Tot el que inclou el pla Hobby, a més…
-                </div>
-                <ul className="space-y-2.5 pt-4">
-                  <li className="flex items-center gap-2">
-                    <Check className="w-4 h-4" />
-                    <span>10.000 crèdits de missatges/mes</span>
-                  </li>
-                  <li className="flex items-center gap-2">
-                    <Check className="w-4 h-4" />
-                    <span>5 xatbots</span>
-                  </li>
-                  <li className="flex items-center gap-2">
-                    <Check className="w-4 h-4" />
-                    <span>3 team members</span>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </CardContent>
-        </Card>
-
-        <Card>
-          <CardHeader className="space-y-1">
-            <CardTitle>
-              <h2 className="text-xl font-bold">Il·limitat</h2>
-            </CardTitle>
-            <div className="flex items-baseline justify-between">
-              <span className="text-4xl font-bold">$399</span>
-              <span className="text-sm text-muted-foreground">Al mes</span>
-            </div>
-          </CardHeader>
-          <CardContent className="h-full">
-            <div className="h-full flex flex-col">
-              <div className="space-y-4">
-                <div className="text-sm text-muted-foreground">
-                  Tot el que inclou el pla Estàndard, a més…
-                </div>
-                <ul className="space-y-2.5 pt-4">
-                  <li className="flex items-center gap-2">
-                    <Check className="w-4 h-4" />
-                    <span>
-                      40.000 crèdits de missatges/mes (els missatges que superin
-                      aquest límit utilitzaran la teva clau d’API d’OpenAI)
-                    </span>
-                  </li>
-                  <li className="flex items-center gap-2">
-                    <Check className="w-4 h-4" />
-                    <span>10 xatbots</span>
-                  </li>
-                  <li className="flex items-center gap-2">
-                    <Check className="w-4 h-4" />
-                    <span>5 membres de l’equip</span>
-                  </li>
-                  <li className="flex items-center gap-2">
-                    <Check className="w-4 h-4" />
-                    <span>Elimina “Powered by Chatbotfor</span>
-                  </li>
-                  <li className="flex items-center gap-2">
-                    <Check className="w-4 h-4" />
-                    <span>Usa els teus propis dominis personalitzats</span>
-                  </li>
-                  <li className="flex items-center gap-2">
-                    <Check className="w-4 h-4" />
-                    <span>Analítiques avançades</span>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </CardContent>
-        </Card>
+            </CardContent>
+          </Card>
+        ))}
       </div>
     </div>
   );
