fb31123df3ff679986bdada4316ce7c0607016e7
Netra Luis Castells <netraluis@gmail.com>
2025-01-10 12:44:21 +0100

    absolute url

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,7 @@ export default async function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
+        <link rel="icon" href="https://app.netraluis.com/favicon-16x16.png" sizes="16x16" type="image/png" />
         <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
         <link rel="icon" href="/favicon-192x192.png" sizes="192x192" type="image/png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
