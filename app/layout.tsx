969888269dfc22afbd98cde838f8f738e64e6b5e
Netra Luis Castells <netraluis@gmail.com>
2025-01-10 13:47:14 +0100

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,7 +76,7 @@ export default async function RootLayout({
 
         <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
         <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
-        <link rel="icon" href="/favicon-192x192.png" sizes="192x192" type="image/png" />
+        <link rel="icon" href="/android-chrome-192x192.png" sizes="192x192" type="image/png" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
       </head>
