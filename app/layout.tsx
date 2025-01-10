f26ab9eefc867120b36ca236b3a7ea7582918f3e
Netra Luis Castells <netraluis@gmail.com>
2025-01-10 17:43:35 +0100

    add ferninadn email

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 "use server";
 import "./globals.css";
 import Script from "next/script";
+import { createClient } from "@/lib/supabase/server";
+import { getUserById } from "@/lib/data/user";
 
 // export const metadata = {
 //   title: "Chatbotfor",
@@ -40,6 +42,20 @@ export default async function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
 
+  const supabase = createClient();
+
+  let userData : {name: string, email: string} = {name: '', email: ''};
+
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  if(!error && user ) {
+    const userLocal = await getUserById(user.id);
+    userData = {name: userLocal.name || '', email: userLocal.email || ''};
+  }
+
   return (
     <html lang="en">
       <body>
@@ -59,8 +75,8 @@ export default async function RootLayout({
             Fernand('init', { appId: 'chatbotfor' });
             Fernand('set', {
               user: {
-                  name: 'Richard',
-                  email: 'richard@piedpiper.com'
+                  name: '${userData.name || ''}',
+                  email: '${userData.email || ''}'
                 }
             });
 
