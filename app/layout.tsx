4f581224ea0afd34f7c4ac00eac63c5ea99d2b3a
Netra Luis Castells <netraluis@gmail.com>
2025-01-10 12:18:23 +0100

    sizes typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon-16x16.png" sizes="32x32" type="image/png" />
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
       </head>
       <body>{children}</body>
     </html>
