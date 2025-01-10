af94931f8783050fde54d02c9356010134b43b2c
Netra Luis Castells <netraluis@gmail.com>
2025-01-10 14:54:24 +0100

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,9 +37,9 @@ export const metadata = {
   description: "Creamos asistentes virtuales para tu negocio",
   icons: {
     icon: [
-      { url: "/favicon.ico", sizes: "any" },
+      // { url: "/favicon.ico", sizes: "any" },
       { url: "/favicon-16x16.png", type: "image/png", sizes: "16x16" },
-      { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
+      // { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
       // { url: "/favicon-192x192.png", type: "image/png", sizes: "192x192" },
     ],
     apple: "/apple-touch-icon.png",
