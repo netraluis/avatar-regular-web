43b51c2d5adc8278fb8394e392cd6c330b9d1624
Netra Luis Castells <netraluis@gmail.com>
2025-02-05 09:19:53 +0100

    onboarding

--- app/app/(dashboard)/layout.tsx
+++ app/app/(dashboard)/layout.tsx
@@ -6,17 +6,29 @@ import {
 } from "@/components/context/dashboardLanguageContext";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
+import { usePathname } from "next/navigation";
 
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
   const {
     state: { userLocal },
   } = useAppContext();
 
+  const split = pathname.split("/")[pathname.split("/").length -1 ]
+
+  // const absolutePath = pathname.split("/").slice(pathname.split("/").length -2, pathname.split("/").length -0).join("/");
+
+
+
   const language: Language = (userLocal?.language as Language) || Language.EN;
+
+  if(split === 'new' || split === 'instructions' || split === 'files'){
+    return <DashboardLanguageProvider userLanguage={language}><div className="flex justify-center h-screen"><div className='flex'>{children}</div></div></DashboardLanguageProvider>
+  }
 
   return (
     <DashboardLanguageProvider userLanguage={language}>
